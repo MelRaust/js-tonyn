78bb03bf866eb119ce085bc790e42e49976759a4
MelRaust <147696053+MelRaust@users.noreply.github.com>
2024-01-09 16:37:17 +0100

    maj

--- dossier.js/script.js
+++ dossier.js/script.js
@@ -1,1 +1,13 @@
-console.log('test')
+function start(){
+    let step1 = document.getElementById('step1')
+    let stepinner1 = document.getElementById('stepinner1')
+    let title = document.getElementById('title')
+    
+    console.log(step1)
+    
+    step1.style.opacity = 1;
+    stepinner1.style.opacity = 1;
+    title.style.opacity = 1;
+    
+    
+}
