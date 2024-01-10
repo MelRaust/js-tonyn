ff0929da153ddd529d56a1480a901975b9fdfe0b
MelRaust <147696053+MelRaust@users.noreply.github.com>
2024-01-10 14:56:15 +0100

    maj

--- dossier.js/script.js
+++ dossier.js/script.js
@@ -12,18 +12,6 @@ function step1(){
     title.style.opacity = 1;
     title.style.top = 0
     title.transition = "2s" 
-    
-    setTimeout(()=>{     
-    
-    step1.style.opacity = 0;   
-    step1.transition = "8s"     
-    step1.style.opacity = 0;     
-    step1.style.transition = "8s"     
-    step1.style.opacity = 0;     
-    step1.style.transition.left = "8s"     
-    step1.style.opacity = 0;     
-    step1.style.transition =   "8s"    
-},"10000");
 }
 
 function step2(){ 
@@ -34,20 +22,14 @@ function step2(){
     console.log(step2)
     
     step2.style.opacity = 1;
+    step2.style.bottom = 0
+    step2.transition = "2s"
     stepinner2.style.opacity = 1;
+    stepinner2.style.left = 0
+    stepinner2.transition = "2s"
     title2.style.opacity = 1;
-    
-    setTimeout(()=>{     
-    
-    step2.style.opacity = 0;     
-    step2.transition = "8s"     
-    step2.style.opacity = 0;     
-    step2.style.transition = "8s"     
-    step2.style.opacity = 0;     
-    step2.style.transition.left = "8s"     
-    step2.style.opacity = 0;     
-    step2.style.transition = "8s"    
-},"10000");
+    title2.style.top = 0
+    title2.transition = "2s" 
 }
 
 function step3(){ 
@@ -58,20 +40,14 @@ function step3(){
     console.log(step3)
     
     step3.style.opacity = 1;
+    step3.style.bottom = 0
+    step3.transition = "2s"
     stepinner3.style.opacity = 1;
+    stepinner3.style.left = 0
+    stepinner3.transition = "2s"
     title3.style.opacity = 1;
-    
-    setTimeout(()=>{     
-    
-    step3.style.opacity = 0;     
-    step3.transition = "8s"     
-    step3.style.opacity = 0;     
-    step3.style.transition = "8s"     
-    step3.style.opacity = 0;     
-    step3.style.transition.left = "8s"     
-    step3.style.opacity = 0;     
-    step3.style.transition =   "8s"    
-},"10000");
+    title3.style.top = 0
+    title3.transition = "2s" 
 }
 
 function step4(){ 
@@ -82,20 +58,14 @@ function step4(){
     console.log(step4)
     
     step4.style.opacity = 1;
+    step4.style.bottom = 0
+    step4.transition = "2s"
     stepinner4.style.opacity = 1;
+    stepinner4.style.left = 0
+    stepinner4.transition = "2s"
     title4.style.opacity = 1;
-    
-    setTimeout(()=>{     
-    
-    step4.style.opacity = 0;     
-    step4.transition = "8s"     
-    step4.style.opacity = 0;     
-    step4.style.transition = "8s"     
-    step4.style.opacity = 0;     
-    step4.style.transition.left = "8s"     
-    step4.style.opacity = 0;     
-    step4.style.transition =   "8s"    
-},"10000");
+    title4.style.top = 0
+    title4.transition = "2s" 
 }
 
 function step5(){ 
@@ -107,21 +77,17 @@ function step5(){
     console.log(step5)
     
     step5.style.opacity = 1;
+    step5.style.bottom = 0
+    step5.transition = "2s"
     stepinner5.style.opacity = 1;
+    stepinner5.style.left = 0
+    stepinner5.transition = "2s"
     stepinner6.style.opacity = 1;
+    stepinner6.style.left = 0
+    stepinner6.transition = "2s"
     title5.style.opacity = 1;
-    
-    setTimeout(()=>{     
-    
-    step5.style.opacity = 0;     
-    step5.transition = "8s"     
-    step5.style.opacity = 0;     
-    step5.style.transition = "8s"     
-    step5.style.opacity = 0;     
-    step5.style.transition.left = "8s"     
-    step5.style.opacity = 0;     
-    step5.style.transition =   "8s"    
-},"10000");
+    title5.style.top = 0
+    title5.transition = "2s" 
 }
 
 
